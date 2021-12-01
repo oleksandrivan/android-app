8d11334d9b348e192523ae13a10b480e92b61fef
Oleksandr Ivanitskyy <oleksandr.ivanitskyy@glovoapp.com>
2021-12-01 16:25:47 +0100

    Login via Google impl.

--- app/src/main/java/edu/pmdm/sharemybike/FirstFragment.java
+++ app/src/main/java/edu/pmdm/sharemybike/FirstFragment.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import edu.pmdm.sharemybike.databinding.FragmentFirstBinding;
 
@@ -30,6 +33,7 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        showWelcome();
         binding.calendarView.setOnDateChangeListener((view1, year, month, dayOfMonth) -> {
             String date = String.format("%d/%d/%d", dayOfMonth, month, year);
             binding.textviewFirst.setText(String.format("Date %s",date));
@@ -40,6 +44,17 @@ public class FirstFragment extends Fragment {
             NavHostFragment.findNavController(FirstFragment.this)
                     .navigate(R.id.action_FirstFragment_to_itemFragment);
         });
+    }
+
+    private void showWelcome() {
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        String name = sharedPref.getString(getString(R.string.name), "");
+        String email = sharedPref.getString(getString(R.string.email), "");
+        String welcomeText = String.format("Welcome %s! [%s]", name, email);
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(getContext(), welcomeText, duration);
+        toast.show();
+
     }
 
     @Override
--- app/src/main/java/edu/pmdm/sharemybike/MainActivity.java
+++ app/src/main/java/edu/pmdm/sharemybike/MainActivity.java
@@ -1,28 +1,67 @@
 package edu.pmdm.sharemybike;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity  {
+    GoogleSignInClient signInClient;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        signInClient = GoogleSignIn.getClient(this, signInOptions);
 
-        Button btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), BikeActivity.class);
-                startActivity(i);
-            }
-        });
+        SignInButton signInButton = findViewById(R.id.signInButton);
+
+        signInButton.setOnClickListener(v -> signIn());
     }
 
+    private void signIn(){
+        Intent signInIntent = signInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInAccount(task);
+        }
+    }
+
+    private void handleSignInAccount(Task<GoogleSignInAccount> task) {
+        GoogleSignInAccount account = task.getResult();
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);;
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.name), account.getDisplayName());
+        editor.putString(getString(R.string.email), account.getEmail());
+        editor.apply();
+        Intent i = new Intent(getApplicationContext(), BikeActivity.class);
+        i.putExtra(getString(R.string.name),account.getDisplayName());
+        i.putExtra(getString(R.string.email),account.getEmail());
+        startActivity(i);
+    }
 }
